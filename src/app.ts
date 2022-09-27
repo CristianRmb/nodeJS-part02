b0d52d21c12c26da16c5349719fe2d85d21f1630
Cristian Rimbu <cristianrimbu@gmail.com>
2022-09-27 23:24:17 +0200

    es Retrive-all-resources

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ const prisma = new PrismaClient();
 
 const app = express();
 
+//Add a route to your API that retrieves all resources.
 app.get("/planets", async (request, response) => {
   const planets = await prisma.planet.findMany();
 
