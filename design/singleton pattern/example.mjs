79fec1979777e60a1eea0bb0792630eb724e8123
Cristian Rimbu <cristianrimbu@gmail.com>
2022-10-03 08:21:02 +0200

    Apply the singleton pattern

---
+++ design/singleton pattern/example.mjs
@@ -0,0 +1,7 @@
+class Print {
+  output(value) {
+    console.log(value);
+  }
+}
+
+export const print = new Print();
---
+++ design/singleton pattern/script-1.mjs
@@ -0,0 +1,3 @@
+import { print } from "./example.mjs";
+
+print.output("test");
